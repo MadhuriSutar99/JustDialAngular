2ad14a6d5da94d546225d1149faf8f4065fb243e
MadhuriSutar <madhurisutar99@gmail.com>
2022-05-18 04:53:10 +0100

    JustDial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { HeadMenuComponent } from './head-menu/head-menu.component';
+import { FootComponent } from './foot/foot.component';
+import { FootMenuComponent } from './foot-menu/foot-menu.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { ListItemsComponent } from './list-items/list-items.component';
+import { ItemComponent } from './item/item.component';
+import {HttpClientModule}from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeadMenuComponent,
+    FootComponent,
+    FootMenuComponent,
+    MainContainerComponent,
+    ListItemsComponent,
+    ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/list-items/list-items.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AnyPtrRecord } from 'dns';
+
+@Component({
+  selector: 'app-list-items',
+  templateUrl: './list-items.component.html',
+  styleUrls: ['./list-items.component.css']
+})
+export class ListItemsComponent implements OnInit {
+
+  myData:any;
+  constructor(private depHttp:HttpClient ) 
+  {
+    depHttp.get("https://reqres.in/api/users?page=2").
+    subscribe((data)=>{this.myData=data})
+
+   }
+
+  ngOnInit(): void 
+  {
+  
+  }
+
+}
